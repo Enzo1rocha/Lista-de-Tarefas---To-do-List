9f00679d71ca815a3a5427ef51032c39afae38b7
Enzo Oliveira da Rocha <Enzo25rocha@gmail.com>
2024-12-21 22:37:32 -0300

    Formulario

---
+++ Javascript/list.js
@@ -0,0 +1,6 @@
+const cancelarForm = window.document.getElementById('Cancelar-formulario-task')
+const ListaDeTarefahtml = window.document.getElementById('ancora-voltar-lista')
+
+cancelarForm.addEventListener('click', () => {
+    ListaDeTarefahtml.click()
+});
