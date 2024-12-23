9761084291fce4e865bf1b1a76f6d57510e1a455
Enzo Oliveira da Rocha <Enzo25rocha@gmail.com>
2024-12-23 11:40:01 -0300

    criando logica renderizar 1

--- Javascript/list.js
+++ Javascript/list.js
@@ -1,16 +1,16 @@
-const { json } = require("sequelize")
+const Data_Atual = new Date();
 
 const cancelarForm = window.document.getElementById('Cancelar-formulario-task')
-const ListaDeTarefahtml = window.document.getElementById('Ancora-voltar-lista')
+const aceitarForm = window.document.getElementById('Adicionar-formulario-task')
+
+const voltar = window.document.getElementById('ancora-voltar-lista')
 const Titulo_input = window.document.getElementById('titulo-task')
 const Desc_input = window.document.getElementById('Descrição-task')
 const Data_input = window.document.getElementById('data-task')
 
-const Tarefas_Salvas = JSON.parse(localStorage.getItem('TarefasDoUsuario'))
 
-const Data_Atual = new Date();
+
 const Dias_da_semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", 'Sexta-Feira', 'Sábado'];
-
 const Meses_do_ano = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
 const Nome_dia = Dias_da_semana[Data_Atual.getDay()];
@@ -18,18 +18,39 @@ const Dia_do_mes = Data_Atual.getDate();
 const Nome_mes = Meses_do_ano[Data_Atual.getMonth()];
 
 const tarefas = {
-    "24/12/2024": [{titulo:'estudar', descricao:'estudar para isso', data:'24/12/2024'}, {titulo: 'dormir', descricao:'dormir as 21:30', data:'22/12/2024'}],
+    "24-12-2024": [{titulo:'estudar', descricao:'estudar para isso', data:'24-12-2024'}, {titulo: 'dormir', descricao:'dormir as 21:30', data:'22-12-2024'}],
 
-    "22/12/2024": [{titulo:'programar', descricao:'criar to do list', data:'26/12/2024'}]
+    "22-12-2024": [{titulo:'programar', descricao:'criar to do list', data:'26-12-2024'}]
 }
 
-localStorage.setItem('TarefasDoUsuario', JSON.stringify(tarefas))
+cancelarForm.addEventListener('click', () => {
+    window.location.href = 'ListaDeTarefas.html'
+})
 
 
-function voltar_Lista_de_Tarefa() {
-    ListaDeTarefahtml.click();
-}
+aceitarForm.addEventListener('submit', () => {
+    criar_nova_tarefa()
+})
 
-function getFormTask() {
+function criar_nova_tarefa() {
+    let dia = `${Dia_do_mes}-${(Data_Atual.getMonth())+1}-${Data_Atual.getFullYear()}`
+
+    let tarefa_para_ser_add = {
+        titulo: String(Titulo_input.value),
+        descricao: String(Desc_input.value),
+        data: String(Data_input.value)
+    }
+
+    let tarefas_salvas = JSON.parse(localStorage.getItem('Tarefas'))
+
+    if (dia in tarefas_salvas) {
+        tarefas_salvas[dia].unshift(tarefa_para_ser_add)
+    } else {
+        tarefas_salvas[dia] = [tarefa_para_ser_add]
+    }
+
+    localStorage.removeItem('Tarefas')
+    localStorage.setItem('Tarefas', JSON.stringify(tarefas_salvas))
+
 
 }
--- Javascript/test.js
+++ Javascript/test.js
@@ -1,11 +1,12 @@
 let tarefas = {
-        "24/12/2024": [{titulo:'estudar', descricao:'estudar para isso', data:'24/12/2024'}, {titulo: 'dormir', descricao:'dormir as 21:30', data:'22/12/2024'}],
+    "24/12/2024": [{titulo:'estudar', descricao:'estudar para isso', data:'24/12/2024'}, {titulo: 'dormir', descricao:'dormir as 21:30', data:'22/12/2024'}],
 
-        "22/12/2024": [{titulo:'programar', descricao:'criar to do list', data:'26/12/2024'}]
+    "22/12/2024": [{titulo:'programar', descricao:'criar to do list', data:'26/12/2024'}]
 }
 
-Object.keys(tarefas).forEach(chave => {
-    console.log(tarefas[chave][0].titulo)
-    console.log(tarefas[chave][0].descricao)
-    console.log(tarefas[chave][0].data)
-});
+
+const dia = '22/12/2024'
+
+let novaTarefa = {titulo: 'bizo', descricao: 'nao sei', data: '24/12/2024'}
+
+tarefas[dia].unshift(novaTarefa)
