c414e607516328d492ebb9260cbb0bdd4968f9a3
Enzo Oliveira da Rocha <Enzo25rocha@gmail.com>
2024-12-23 13:06:55 -0300

    logica 2

--- Javascript/list.js
+++ Javascript/list.js
@@ -1,27 +1,17 @@
 const Data_Atual = new Date();
-
 const cancelarForm = window.document.getElementById('Cancelar-formulario-task')
 const aceitarForm = window.document.getElementById('Adicionar-formulario-task')
-
 const voltar = window.document.getElementById('ancora-voltar-lista')
 const Titulo_input = window.document.getElementById('titulo-task')
 const Desc_input = window.document.getElementById('Descrição-task')
 const Data_input = window.document.getElementById('data-task')
-
-
-
 const Dias_da_semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", 'Sexta-Feira', 'Sábado'];
 const Meses_do_ano = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
-
 const Nome_dia = Dias_da_semana[Data_Atual.getDay()];
 const Dia_do_mes = Data_Atual.getDate();
 const Nome_mes = Meses_do_ano[Data_Atual.getMonth()];
 
-const tarefas = {
-    "24-12-2024": [{titulo:'estudar', descricao:'estudar para isso', data:'24-12-2024'}, {titulo: 'dormir', descricao:'dormir as 21:30', data:'22-12-2024'}],
-
-    "22-12-2024": [{titulo:'programar', descricao:'criar to do list', data:'26-12-2024'}]
-}
+renderizar_tarefas()
 
 cancelarForm.addEventListener('click', () => {
     window.location.href = 'ListaDeTarefas.html'
@@ -51,6 +41,56 @@ function criar_nova_tarefa() {
 
     localStorage.removeItem('Tarefas')
     localStorage.setItem('Tarefas', JSON.stringify(tarefas_salvas))
+}
 
+function renderizar_tarefas() {
 
+    const tarefas_salvas = JSON.parse(localStorage.getItem('Tarefas')) || {};
+    const section_tarefas = window.document.getElementById('section-tarefas')
+
+    for (const tarefas_dos_dias in tarefas_salvas) {
+        if (Object.prototype.hasOwnProperty.call(tarefas_salvas, tarefas_dos_dias)) {
+            const objeto_tarefas = tarefas_salvas[tarefas_dos_dias];
+
+            let div_tarefas_list = window.document.createElement('div');
+            div_tarefas_list.className = 'tarefas-list';
+            section_tarefas.appendChild(div_tarefas_list);
+
+            //pegando nomes de dias e meses
+            let [dia, mes, ano] = tarefas_dos_dias.split('-');
+            let dataObject = new Date(`${ano}-${mes}-${dia}`);
+
+            //html
+            let title_dia = document.createElement('h3');
+            title_dia.className = 'dias-tarefa';
+            title_dia.textContent = `${Dias_da_semana[dataObject.getDay()]}, ${dia} ${Meses_do_ano[mes-1]}`
+            div_tarefas_list.appendChild(title_dia)
+            //html
+
+            for (const key in objeto_tarefas) {
+                if (Object.prototype.hasOwnProperty.call(objeto_tarefas, key)) {
+
+                    const valores_por_objeto = objeto_tarefas[key];
+
+                    let title = valores_por_objeto.titulo;
+                    let desc = valores_por_objeto.descricao;
+                    let date = valores_por_objeto.data;
+
+                    let div_class_tarefas = window.document.createElement('div');
+                    div_class_tarefas.className = 'tarefas';
+
+                    //html
+
+                    div_class_tarefas.innerHTML = `
+                    <h1>${title}</h1>
+                    <p>${desc}</p> 
+                    <h2>Data Limite: ${date}</h2>`  
+
+                    //html
+
+                    div_tarefas_list.appendChild(div_class_tarefas)   
+                }
+            }
+        }
+    }
 }
--- Javascript/test.js
+++ Javascript/test.js
@@ -5,8 +5,26 @@ let tarefas = {
 }
 
 
-const dia = '22/12/2024'
+for (const date in tarefas) {
+    if (Object.prototype.hasOwnProperty.call(tarefas, date)) {
+        const element = tarefas[date]
+        console.log(date);
 
-let novaTarefa = {titulo: 'bizo', descricao: 'nao sei', data: '24/12/2024'}
+        let div_tarefas_list = window.document.createElement('div')
+        
+        div_tarefas_list.innerHTML = '<h3 class="dias-tarefa"></h3>'
+        
+        for (const key in element) {
+            if (Object.prototype.hasOwnProperty.call(element, key)) {
+                const valores = element[key];
+                let title = valores.titulo
+                let desc = valores.descricao
+                let date = valores.data
+                console.log(`${title}, ${desc}, ${date}`)
+                
+                
+            }
+        }
 
-tarefas[dia].unshift(novaTarefa)
+        }
+    }
